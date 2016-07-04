a0af4a59653aa3010896d545d7f6352d2833f178
giwooKang <rkdrldnsla@naver.com>
2016-07-04 14:49:22 +0900

    MVVM추가
    마지막

---
+++ simpleMVVM/simpleMVVM/MainWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace simpleMVVM
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int count;
+
+        public int Count {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                count = value;
+                OnPropertyChanged("Count");
+            }
+        }
+
+        public ICommand IncreseCommand{ get; set; }
+
+        public MainWindowViewModel()
+        {
+            IncreseCommand = new RelayCommand(increseCommand);
+            Count = 0;
+        }
+
+        private void increseCommand(object obj)
+        {
+            Count++;
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ simpleMVVM/simpleMVVM/RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace simpleMVVM
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action<object> _executeAction;
+        private Predicate<object> _canExecuteAction;
+
+        public RelayCommand(Action<object> executeAction, Predicate<object> canExecuteAction = null)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = canExecuteAction;
+        }
+        
+        public bool CanExecute(object parameter)
+        {
+            if(_canExecuteAction==null)
+            {
+                return true;
+            }
+            else
+            {
+                return CanExecute(parameter);
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeAction(parameter);
+        }
+    }
+}
+
+
